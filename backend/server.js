735684577fc2920c951a3aa3ecfa71a110f15ef5
Dhruv Madaan <madaandhruv50@gmail.com>
2024-12-17 19:14:16 +0530

    Replaced urls

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ dotenv.config();
 //Middleware
 app.use(express.json())
 app.use(cors())
-app.use(cookieParser())
+app.use(cookieParser({
+    origin: 'http://localhost:5173',
+    Credential: true
+}))
 
 connectDB()
 
