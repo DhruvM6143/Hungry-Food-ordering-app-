d28af6d03ce02f41e7938bbf68f3c2f9d06d7245
DhruvM6143 <dhruvmm098@gmail.com>
2024-12-17 21:04:56 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = "https://hungry-food-ordering-app-backend.vercel.app"
+    const url = "https://hungry-food-ordering-app-backend.onrender.com"
     const [token, setToken] = useState("")
 
     const [food_list, setFoodList] = useState([]);
