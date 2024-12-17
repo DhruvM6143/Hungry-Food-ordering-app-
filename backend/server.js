789efe7457b718cb7dc1cfc0678a3c694cf52e58
Dhruv Madaan <madaandhruv50@gmail.com>
2024-12-17 19:31:56 +0530

    small error

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 4000;
 dotenv.config();
 //Middleware
 app.use(express.json())
-app.use(cors())
-app.use(cookieParser({
+app.use(cors({
     origin: 'https://hungry-food-ordering-app-frontend.vercel.app',
-    Credential: true
+    credential: true
 }))
+app.use(cookieParser())
 
 connectDB()
 
