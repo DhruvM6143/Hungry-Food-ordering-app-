bed6d29eb63b12377929654566ff0e069df74c98
DhruvM6143 <dhruvmm098@gmail.com>
2024-12-17 21:05:59 +0530

    Update assets.js

--- Admin/src/assets/assets.js
+++ Admin/src/assets/assets.js
@@ -16,4 +16,4 @@ export const assets = {
     parcel_icon
 }
 
-export const url = 'https://hungry-food-backend-mu3e.onrender.com'
+export const url = 'https://hungry-food-ordering-app-backend.onrender.com'
